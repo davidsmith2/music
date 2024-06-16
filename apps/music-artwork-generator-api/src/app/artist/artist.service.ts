c710707e708d71917f22e815da58c6d6386c2027
David Smith <davidsmith2@gmail.com>
2024-06-16 15:21:46 -0400

    display tracks on artist detail

--- apps/music-artwork-generator-api/src/app/artist/artist.service.ts
+++ apps/music-artwork-generator-api/src/app/artist/artist.service.ts
@@ -9,19 +9,7 @@ export class ArtistService {
   getArtists(): Promise<Array<Artist>> {
     const jsonStr = readFileSync(join(__dirname, 'assets', 'Library.json'), 'utf8');
     const json = JSON.parse(jsonStr);
-    return json.artists.map((artist: Artist) => {
-      return {
-        name: artist.name,
-        albums: artist.albums.map((album: Album) => {
-          return {
-            title: album.title,
-            artist: album.artist,
-            cover: album.cover
-          };
-        })
-
-      };
-    });
+    return json.artists;
   }
 
 }
