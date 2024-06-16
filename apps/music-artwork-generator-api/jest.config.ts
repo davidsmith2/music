34111e1c29dcc471a83aeeb2caece0353906caaf
David Smith <davidsmith2@gmail.com>
2024-06-15 21:27:21 -0400

    nx 13 to 14

--- apps/music-artwork-generator-api/jest.config.ts
+++ apps/music-artwork-generator-api/jest.config.ts
@@ -1,6 +1,6 @@
 module.exports = {
   displayName: 'music-artwork-generator-api',
-  preset: '../../jest.preset.js',
+
   globals: {
     'ts-jest': { tsconfig: '<rootDir>/tsconfig.spec.json' },
   },
@@ -10,4 +10,5 @@ module.exports = {
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageDirectory: '../../coverage/apps/music-artwork-generator-api',
   testEnvironment: 'node',
+  preset: '../../jest.preset.ts',
 };
