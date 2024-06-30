f7986384b20b514c3a144deff1b6e8c9adf512e1
David Smith <davidsmith2@gmail.com>
2024-06-30 11:25:15 -0400

    Get duration from mp4

--- tools/scripts/get-file-metadata.sh
+++ tools/scripts/get-file-metadata.sh
@@ -3,13 +3,19 @@
 # Use the first argument as FILE_PATH
 FILE_PATH="$1"
 
-# Extract metadata using AtomicParsley
-metadata=$(AtomicParsley "$FILE_PATH" -t | grep -E 'Atom "(©nam|©ART|©alb)" contains: ')
+# Extract metadata for title, artist and album using AtomicParsley
+metadata1=$(AtomicParsley "$FILE_PATH" -t 1 | grep -E 'Atom "(©nam|©ART|©alb)" contains: ')
 
 # Parse and format the output
-title=$(echo "$metadata" | grep '©nam' | awk -F": " '{print $2}')
-artist=$(echo "$metadata" | grep '©ART' | awk -F": " '{print $2}')
-album=$(echo "$metadata" | grep '©alb' | awk -F": " '{print $2}')
+title=$(echo "$metadata1" | grep '©nam' | awk -F": " '{print $2}')
+artist=$(echo "$metadata1" | grep '©ART' | awk -F": " '{print $2}')
+album=$(echo "$metadata1" | grep '©alb' | awk -F": " '{print $2}')
+
+# Extract metadata for duration using AtomicParsley
+metadata2=$(AtomicParsley "$FILE_PATH" -T 1)
+
+# Parse and format the output
+duration=$(echo "$metadata2" | awk '/Movie duration:/ {print $3}')
 
 # Format as JSON
-echo "{\"title\": \"$title\", \"artist\": \"$artist\", \"album\": \"$album\"}"
+echo "{\"title\": \"$title\", \"artist\": \"$artist\", \"album\": \"$album\", \"duration\": \"$duration\"}"
