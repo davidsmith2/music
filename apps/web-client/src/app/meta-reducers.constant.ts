ef49aa22d6ab5ec14e1172ea613c6924bf2e6c23
David Smith <davidsmith2@gmail.com>
2024-08-11 14:06:14 -0400

    Fix web client

--- apps/web-client/src/app/meta-reducers.constant.ts
+++ apps/web-client/src/app/meta-reducers.constant.ts
@@ -1,13 +1,6 @@
-import { ActionReducer, MetaReducer } from '@ngrx/store';
-import { storeLogger } from 'ngrx-store-logger';
+import { MetaReducer } from '@ngrx/store';
 import { ngrxEntityRelationshipReducer } from 'ngrx-entity-relationship';
 
-export function logger(reducer: ActionReducer<any>): any {
-  // Customize the logger
-  return storeLogger()(reducer);
-}
-
 export const metaReducers: MetaReducer<any>[] = [
-  logger,
   ngrxEntityRelationshipReducer,
 ];
